5243fe18747c89c29096f395821571a697b19905
Michal Kruczek <michal@clearcode.eu>
2017-05-04 22:25:36 +0200

    Switched to ApiKernel. But how to switch between multiple kernels?

--- web/app.php
+++ web/app.php
@@ -6,7 +6,8 @@ use Symfony\Component\HttpFoundation\Request;
 $loader = require __DIR__.'/../app/autoload.php';
 include_once __DIR__.'/../var/bootstrap.php.cache';
 
-$kernel = new AppKernel('prod', false);
+
+$kernel = new ApiKernel('prod', false);
 $kernel->loadClassCache();
 //$kernel = new AppCache($kernel);
 
